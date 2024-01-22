178d058f7a49ff49b89e30ec6b1dec7e9418ae32
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-22 17:41:38 +0100

    next_prev_buttons_fixed

--- js/script.js
+++ js/script.js
@@ -10,18 +10,26 @@ const app = createApp ({
     }),
     computed: {
         lastElementIndex(){
-            return this.destinations.length - 1
+            return this.destinations.length - 1;
         },
         isFirstIndex(){
-            return this.currentIndex === 0
+            return this.currentIndex === 0;
         },
         isLastIndex(){
-            return this.currentIndex === this.lastElementIndex
+            return this.currentIndex === this.lastElementIndex;
         }
     },
-    // methods: {
-
-    // }
+    methods: {
+        setCurrentIndex(stringa){
+            if (stringa === "prev") {
+                if (this.isFirstIndex) this.currentIndex = this.lastElementIndex;
+                else this.currentIndex--;
+            } else if (stringa === "next") {
+                if (this.isLastIndex) this.currentIndex = 0;
+                else this.currentIndex++;
+            } else {this.currentIndex = stringa;}
+        }
+    }
 })
 
 app.mount("#root");
