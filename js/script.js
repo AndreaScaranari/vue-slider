d7cf8f88e57005bb4a867af23f9c85ddc33324ae
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-22 16:56:43 +0100

    thumbnails_fixed

--- js/script.js
+++ js/script.js
@@ -7,7 +7,21 @@ const app = createApp ({
     data: () => ({
         destinations,
         currentIndex: 0
-    })
+    }),
+    computed: {
+        lastElementIndex(){
+            return this.destinations.length - 1
+        },
+        isFirstIndex(){
+            return this.currentIndex === 0
+        },
+        isLastIndex(){
+            return this.currentIndex === this.lastElementIndex
+        }
+    },
+    // methods: {
+
+    // }
 })
 
 app.mount("#root");
