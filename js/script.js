c9d2e88382f122f8e22f1d52cc69c754eda6a13f
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-22 16:41:30 +0100

    gallery_fixed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hey!");
+// check di prova
+console.log("Hey Vue!", Vue);
+
+const { createApp } = Vue;
+
+const app = createApp ({
+    data: () => ({
+        destinations,
+        currentIndex: 0
+    })
+})
+
+app.mount("#root");
